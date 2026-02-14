6cb2f67a8cecb562796563486b6229af0b2a6449
DionysusBenstein <dionysusbenstein@gmail.com>
2026-02-14 15:04:00 +0300

    Fix modules error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,15 @@ import { AuthService } from './services/auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Password } from 'src/users/entities/password.entity';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { DbTransactionFactory } from 'src/database/transaction-factory';
 import { Session } from './entities/session.entity';
 import { PasswordResetToken } from './entities/password-reset-token.entity';
 import { VerificationCode } from './entities/verification-code.entity';
 import { MailModule } from 'src/mail/mail.module';
-import { CustomJwtService } from './services/jwt.service';
 import { VerificationCodeService } from './services/verification-code.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtSharedModule } from './jwt-shared.module';
 
 @Module({
   imports: [
@@ -24,13 +23,7 @@ import { UsersModule } from 'src/users/users.module';
       PasswordResetToken,
       VerificationCode,
     ]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-      }),
-      inject: [ConfigService],
-    }),
+    JwtSharedModule,
     MailModule,
     UsersModule,
   ],
@@ -39,7 +32,6 @@ import { UsersModule } from 'src/users/users.module';
     AuthService,
     ConfigService,
     DbTransactionFactory,
-    CustomJwtService,
     VerificationCodeService,
   ],
   exports: [AuthService],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Password } from './entities/password.entity';
 import { DbTransactionFactory } from 'src/database/transaction-factory';
+import { JwtSharedModule } from 'src/auth/jwt-shared.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Password]),
+    JwtSharedModule,
   ],
   controllers: [UsersController],
   providers: [UsersService, DbTransactionFactory],
